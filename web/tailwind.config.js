9e592cd56ac8189d038f66747a42ae18fe800118
sereneblue <sereneblue@users.noreply.github.com>
2020-09-07 22:30:15 -0400

    build: update tailwind config

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -6,11 +6,22 @@ module.exports = {
     ]
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-300": '#1d1f21',
+        "dark-text-100": '#a5a8a7'
+      }
+    },
   },
   variants: {},
   plugins: [],
   future: {
     removeDeprecatedGapUtilities: true,
+  },
+  dark: 'class',
+  experimental: {
+    extendedSpacingScale: true,
+    applyComplexClasses: true,
+    darkModeVariant: true,
   },
 }
