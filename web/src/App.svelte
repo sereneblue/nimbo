d84ac153d482f349ad1f8ccfd6aebfc599eed8bd
sereneblue <sereneblue@users.noreply.github.com>
2020-09-27 15:14:19 -0400

    feat: add context for nimbo store

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import { setContext } from 'svelte';
 	import { Router, Route } from "svelte-routing";
 	
 	import TailwindCSS from "./style/TailwindCSS.svelte"
-
 	import { Board, Card, Home } from "./routes";
+	import { nimboStore } from "./datastore/stores";
+
+	let loading: Promise<boolean> = $nimboStore.init();
+
+	setContext('nimbo', nimboStore);
 
 	let url: string = "";
 </script>
@@ -12,14 +17,16 @@
 
 <Router url="{url}">
 	<div class="dark">
-		<Route path="c/:cardId" let:params>
-			<Card id={params.cardId} />
-		</Route>
+		{#await loading then done}
+			<Route path="c/:cardId" let:params>
+				<Card id={params.cardId} />
+			</Route>
 
-		<Route path="b/:boardId" let:params>
-			<Board id={params.boardId} />
-		</Route>
+			<Route path="b/:boardId" let:params>
+				<Board id={params.boardId} />
+			</Route>
 
-		<Route path="/" component="{Home}" />
+			<Route path="/" component="{Home}" />
+		{/await}
 	</div>
 </Router>
