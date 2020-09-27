ed03378022b279dc8126ce8eabf4bcc829fe0232
sereneblue <sereneblue@users.noreply.github.com>
2020-09-27 16:58:12 -0400

    feat(board): add store/context and update board last view time

--- web/src/routes/Board.svelte
+++ web/src/routes/Board.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+  import { getContext } from 'svelte';
+
+  import type nimbo from '../nimbo';
+
   export let id: string;
+
+  const nimboStore: Writable<nimbo> = getContext('nimbo');
+
+  let boardIndex: number = $nimboStore.boards.findIndex(b => b.id === id);
+
+  if (boardIndex > -1) {
+    $nimboStore.updateViewTime($nimboStore.boards[boardIndex].id);
+  }
 </script>
 
-<div class="flex flex-col max-h-screen h-screen overflow-hidden bg-light-300 dark:bg-dark-300 text-light dark:text-dark py-2 px-4">
+<div class="flex flex-col max-h-screen h-screen overflow-hidden bg-light-300 dark:bg-dark-300 text-light dark:text-dark">
   {id}
 </div>
