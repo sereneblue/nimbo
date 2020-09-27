ba8e9aa8209d0b98194cc60c3f196a1f8a86747b
sereneblue <sereneblue@users.noreply.github.com>
2020-09-27 15:16:11 -0400

    feat(modal): add action to capture escape key to close

--- web/src/components/base/Modal.svelte
+++ web/src/components/base/Modal.svelte
@@ -12,10 +12,26 @@
       dispatch("close");
     });
   }
+
+  const init = (): object => {
+    const handleKeyPress = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        dispatch("close");
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress)
+
+    return {
+      destroy() {
+        window.removeEventListener("keydown", handleKeyPress);
+      }
+    }
+  };
 </script>
 
 {#if isOpen }
-  <div class="absolute w-full top-0 h-screen z-10 bg-dark-300-75" transition:fade={{ duration: 175 }}>
+  <div use:init class="absolute w-full top-0 h-screen z-10 bg-dark-300-75" transition:fade={{ duration: 175 }}>
     <div class="bg-light-200 mt-24 w-3/4 md:w-2/3 xl:w-1/2 mx-auto rounded py-2 shadow-lg dark:bg-dark-200" use:handleClickOutside>
     	<slot isOpen={isOpen}></slot>
     </div>
