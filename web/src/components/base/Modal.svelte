359bd5f16e4e7584c9c38fafa2c4ea96f68d6ff6
sereneblue <sereneblue@users.noreply.github.com>
2020-09-28 23:14:46 -0400

    fix(modal): add class to pin modal to left

--- web/src/components/base/Modal.svelte
+++ web/src/components/base/Modal.svelte
@@ -31,7 +31,7 @@
 </script>
 
 {#if isOpen }
-  <div use:init class="absolute w-full top-0 h-screen z-10 bg-dark-300-75" transition:fade={{ duration: 175 }}>
+  <div use:init class="absolute w-full top-0 left-0 h-screen z-10 bg-dark-300-75" transition:fade={{ duration: 175 }}>
     <div class="bg-light-200 mt-24 w-3/4 md:w-2/3 xl:w-1/2 mx-auto rounded py-2 shadow-lg dark:bg-dark-200" use:handleClickOutside>
     	<slot isOpen={isOpen}></slot>
     </div>
