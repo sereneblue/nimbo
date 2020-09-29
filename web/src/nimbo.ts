9b727cbc9cffd639eb0f04909d44b8786b7861aa
sereneblue <sereneblue@users.noreply.github.com>
2020-09-28 22:46:41 -0400

    feat(nimbo): add board and list related functions

--- web/src/nimbo.ts
+++ web/src/nimbo.ts
@@ -1,7 +1,8 @@
 import { nimboDB } from './datastore/db';
 import Board from './datastore/models/Board';
-import type List from './datastore/models/List';
+import List from './datastore/models/List';
 import type Card from './datastore/models/Card';
+import type { SwapObject } from './types';
 
 export default class nimbo {
   db: nimboDB;
@@ -42,6 +43,17 @@ export default class nimbo {
     this.showCommandPalette = false;
   }
 
+  public async addNewList(boardId: string): Promise<void> {
+    let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
+    
+    if (boardIndex > -1) {
+      let l: List = new List(boardId, this.boards[boardIndex].lists.length + 1); 
+      this.boards[boardIndex].lists = [...this.boards[boardIndex].lists, l];
+
+      await this.db.lists.add(l);
+    }
+  }
+
   public async createBoard(title: string): Promise<string> {
     let b: Board = new Board(title);
     this.boards = [...this.boards, b];
@@ -61,16 +73,88 @@ export default class nimbo {
     await this.db.boards.delete(id);
   }
 
+  public async deleteList(boardId: string, listId: string): Promise<void> {
+    let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
+    
+    if (boardIndex > -1) {
+      let listIndex: number = this.boards[boardIndex].lists.findIndex(l => l.id === listId); 
+
+      if (listIndex > -1) {
+        this.boards[boardIndex].lists.splice(listIndex, 1);
+
+        await this.db.lists.delete(listId);
+      }
+    }
+  }
+
+  public getBoard(boardId: string): Board | null {
+    let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
+
+    if (boardIndex > -1) {
+      return this.boards[boardIndex];
+    }
+
+    return null;
+  }
+
+  public async swapLists(boardId: string, l: SwapObject): Promise<void> {
+    let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
+
+    if (boardIndex > -1) {
+      this.boards[boardIndex].swapLists(l);
+
+      await this.db.transaction('rw', this.db.lists, async (): Promise<void> =>{
+        let [from, to]: List[] = await Promise.all([
+          this.db.lists.get({ boardId, index: l.from }),
+          this.db.lists.get({ boardId, index: l.to })
+        ]);
+        
+        from.index = l.to;
+        to.index = l.from;
+
+        await this.db.lists.bulkPut([from, to]);
+      });
+    }
+  }
+
   public async toggleBoardStar(boardId: string): Promise<void> {
     let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
 
     if (boardIndex > -1) {
-      this.boards[boardIndex].isStarred = !this.boards[boardIndex].isStarred;
-    }
+      this.boards[boardIndex].toggleBoardStar();
 
-    await this.db.boards.update(boardId, {
-      isStarred: this.boards[boardIndex].isStarred
-    });
+      await this.db.boards.update(boardId, {
+        isStarred: this.boards[boardIndex].isStarred
+      });
+    }
+  }
+
+  public async updateBoardTitle(boardId: string, title: string): Promise<void> {
+    let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
+
+    if (boardIndex > -1) {
+      this.boards[boardIndex].setTitle(title);
+
+      await this.db.boards.update(boardId, {
+        title
+      });
+    }
+  }
+
+  public async updateListTitle(boardId: string, listId: string, title: string): Promise<void> {
+    let boardIndex: number = this.boards.findIndex(b => b.id === boardId);
+    
+    if (boardIndex > -1) {
+      let listIndex: number = this.boards[boardIndex].lists.findIndex(l => l.id === listId); 
+
+      if (listIndex > -1) {
+        this.boards[boardIndex].lists[listIndex].setTitle(title);
+
+        await this.db.lists.update(listId, {
+          title
+        });
+      }
+    }
   }
 
   public async updateViewTime(boardId: string): Promise<void> {
