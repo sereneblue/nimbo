723fa696f54e4375fc24a2c203dd79f392e90dbe
sereneblue <sereneblue@users.noreply.github.com>
2020-09-30 22:31:52 -0400

    feat: add new type for sorting

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,3 +1,15 @@
+export type SortObject = {
+  boardId: string,
+  from: {
+    list: string,
+    index: number
+  },
+  to: {
+    list: string,
+    index: number
+  }
+}
+
 export type SwapObject = {
   from: number;
   to: number;
