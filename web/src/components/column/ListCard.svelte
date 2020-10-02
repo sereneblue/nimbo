5b917f255c8489d23f5b9646b7b0930b19ddd3b1
sereneblue <sereneblue@users.noreply.github.com>
2020-10-01 20:08:53 -0400

    feat(list): add list card component

---
+++ web/src/components/column/ListCard.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import type Card from '../../datastore/models/Card';
+
+  export let c: Card;
+
+  const handleCardClick = (e: Event): void => {
+
+  }
+</script>
+
+<div class="card cursor-pointer focus:outline-none focus:border-white border border-transparent shadow font-semibold p-2 rounded-sm" on:click={handleCardClick} tabindex="0">
+  <div class="break-words">
+    {c.title}
+  </div>
+</div>
