2642131223b14ed29e010f1236837435fd331acd
sereneblue <sereneblue@users.noreply.github.com>
2020-10-07 18:55:29 -0400

    feat(util): add function to format dates

--- web/src/util/index.ts
+++ web/src/util/index.ts
@@ -1,5 +1,8 @@
 import { customAlphabet } from 'nanoid';
 
+let d: Date = new Date();
+let currentYear: number = new Date().getFullYear();
+
 const clickOutside = (node: Node, closeAction: Function): object => {
   const handleClick = (event: Event) => {
     let path = event.composedPath();
@@ -20,9 +23,20 @@ const clickOutside = (node: Node, closeAction: Function): object => {
   };
 };
 
+const formatDate = (timestamp: number): string => {
+  d.setTime(timestamp);
+  
+  if (d.getFullYear() == currentYear) {
+    return d.toLocaleString('en', { month: 'short', day: 'numeric' });
+  }
+
+  return d.toLocaleString('en', { month: 'short', day: 'numeric', year: 'numeric' });
+}
+
 const nanoid = customAlphabet('2346789ABCDEFGHJKLMNPQRTUVWXYZabcdefghijkmnpqrtwxyz', 10);
 
 export {
   clickOutside,
+  formatDate,
   nanoid
 }
