5d7ae6e29aba4c454978c11a37bf8139389287df
sereneblue <sereneblue@users.noreply.github.com>
2020-10-09 22:03:52 -0400

    feat(util): add function to format seconds

--- web/src/util/index.ts
+++ web/src/util/index.ts
@@ -33,10 +33,33 @@ const formatDate = (timestamp: number): string => {
   return d.toLocaleString('en', { month: 'short', day: 'numeric', year: 'numeric' });
 }
 
+const formatTime = (seconds: number): string => {
+  let t: string = "";
+
+  let hrs: number = Math.floor(seconds / 3600);
+  let min: number = Math.floor((seconds % 3600) / 60);
+  let sec: number = seconds % 60;
+
+  if (hrs) {
+    t += `${hrs}h `;
+  }
+
+  if (min) {
+    t += `${min}m `;
+  }
+
+  if (sec) {
+    t += `${sec}s`;
+  }
+
+  return t;
+}
+
 const nanoid = customAlphabet('2346789ABCDEFGHJKLMNPQRTUVWXYZabcdefghijkmnpqrtwxyz', 10);
 
 export {
   clickOutside,
   formatDate,
+  formatTime,
   nanoid
 }
