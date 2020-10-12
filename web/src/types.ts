57abb15a541bc7575ea320eba27ddf6dfb9d28cb
sereneblue <sereneblue@users.noreply.github.com>
2020-10-12 19:01:20 -0400

    feat(types): add additional types

--- web/src/types.ts
+++ web/src/types.ts
@@ -1,3 +1,29 @@
+import type Board from "./datastore/models/Board";
+import type Card from "./datastore/models/Card";
+import type List from "./datastore/models/List";
+
+export type CardDetails = {
+  board: Board;
+  list: List;
+  card: Card;
+}
+
+export type ChecklistItem = {
+  text: string;
+  checked: boolean;
+}
+
+export enum RESULT_TYPE {
+  BOARD,
+  CARD,
+};
+
+export type SearchObject = {
+  type: RESULT_TYPE;
+  text: string;
+  path: string;
+}
+
 export type SortObject = {
   boardId: string,
   from: {
@@ -13,4 +39,10 @@ export type SortObject = {
 export type SwapObject = {
   from: number;
   to: number;
+}
+
+export type TimeLogItem = {
+  date: number;
+  description: string;
+  duration: number;
 }
