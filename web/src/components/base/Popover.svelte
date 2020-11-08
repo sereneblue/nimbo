73afb98f8b3e573bee1b27266e917748b851755a
sereneblue <sereneblue@users.noreply.github.com>
2020-11-08 15:20:22 -0500

    feat: add popover component

---
+++ web/src/components/base/Popover.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { fade } from 'svelte/transition';
+  import { clickOutside } from "../../util";
+
+  export let isOpen: boolean;
+
+  const dispatch = createEventDispatcher();
+  
+  const handleClickOutside = (node: Node): object => {
+    return clickOutside(node, () => {
+      if (isOpen) {
+        dispatch("close");
+      }
+    });
+  }
+</script>
+
+{#if isOpen }
+  <div use:handleClickOutside class="absolute mt-2 z-10 rounded-md bg-light-100 dark:bg-dark-200 text-light dark:text-dark shadow-xl w-auto">
+	 <slot isOpen={isOpen}></slot>
+  </div>
+{/if}
