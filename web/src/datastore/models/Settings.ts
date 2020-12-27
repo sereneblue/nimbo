8e6113d2501b26474e0f9ba9365ed86ab438eac4
sereneblue <sereneblue@users.noreply.github.com>
2020-12-27 17:08:12 -0500

    feat(settings): add list filter to settings

--- web/src/datastore/models/Settings.ts
+++ web/src/datastore/models/Settings.ts
@@ -3,6 +3,7 @@ import type { DueFilterOptions, Theme } from '../../types';
 export default class Settings {
   id: number;
   dueFilter: DueFilterOptions;
+  listFilter: string;
   maxCards: number;
   theme: Theme;
   selectedBoards: string[];
@@ -12,6 +13,7 @@ export default class Settings {
     this.theme = "dark";
     this.maxCards = 5;
     this.dueFilter = "all";
+    this.listFilter = "";
     this.selectedBoards = ["all"];
   }
 }
