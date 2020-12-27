1d93017e51a15bfd2714b316512a7b7624701804
sereneblue <sereneblue@users.noreply.github.com>
2020-12-27 15:49:38 -0500

    feat(list card): add time tracking for cards

--- web/src/components/column/ListCard.svelte
+++ web/src/components/column/ListCard.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { fly } from 'svelte/transition';
   import type Card from '../../datastore/models/Card';
   import { PRIORITY, LABEL_COLOR } from '../../types';
-  import { formatDate } from '../../util';
+  import type { TimeEntry } from '../../types';
+  import { formatDate, formatForStopwatch, formatTime } from '../../util';
   
   export let c: Card;
+  export let isZen: boolean = false;
 
-  const handleTimeTracking = (e: Event): void => {
+  const dispatch = createEventDispatcher();
+
+  let seconds: number = 0;
+  let stopwatchInterval: number;
+  let stopwatchStarted: boolean = false;
+
+  const handleStopwatchStart = (e: Event): void => {
     e.stopPropagation();
+    
+    clearInterval(stopwatchInterval);
+
+    if (stopwatchStarted) {
+      card.trackTime(seconds);
+
+      dispatch("trackTime", card);
+
+      seconds = 0;
+      stopwatchStarted = false;
+    } else {
+      stopwatchStarted = true;
+      stopwatchInterval = setInterval(() => {
+        seconds++;
+      }, 1000)
+    }
   }
 
   $: card = c;
-
-  $: hasMoreDetails = !!card.checklist.length || !!card.due || !!card.description || card.priority in PRIORITY;
   $: checklistCompleted = card.checklist.filter(item => item.checked).length;
+  $: totalTrackedTime = card.log.reduce((a: TimeEntry, b: TimeEntry): number => a + b.duration, 0);
+  $: hasMoreDetails = !!card.checklist.length || !!card.log.length || !!card.due || !!card.description || card.priority in PRIORITY;
 </script>
 
 <div class="card card-label relative group cursor-pointer focus:outline-none focus:border-white border border-transparent shadow font-semibold p-2 rounded-sm" tabindex="0">
@@ -34,20 +60,26 @@
   </div>
   {#if hasMoreDetails}
     <div class="flex justify-between group mt-2 text-sm -mb-1 opacity-75">
-      <div class="flex items-center">
+      <div class="flex items-center space-x-1.5">
         {#if card.description}
-          <div class="mr-2">
+          <div class="py-1">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M17 10H3M21 6H3M21 14H3M17 18H3"/></svg>
           </div>
         {/if}
         {#if card.checklist.length}
-          <div class={checklistCompleted == card.checklist.length ? "flex items-center p-1 rounded mr-2 text-white bg-green-500" : "flex items-center p-1 rounded mr-1"}>
+          <div class={checklistCompleted == card.checklist.length ? "flex items-center rounded text-white bg-green-500 py-1" : "flex items-center rounded py-1"}>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 11-5.93-9.14"/><path d="M22 4L12 14.01l-3-3"/></svg>
             <span class="ml-1">{checklistCompleted} / {card.checklist.length}</span>
           </div>
         {/if}
+        {#if card.log.length}
+          <div class="flex items-center rounded py-1">
+            <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="7"></circle><polyline points="12 9 12 12 13.5 13.5"></polyline><path d="M16.51 17.35l-.35 3.83a2 2 0 0 1-2 1.82H9.83a2 2 0 0 1-2-1.82l-.35-3.83m.01-10.7l.35-3.83A2 2 0 0 1 9.83 1h4.35a2 2 0 0 1 2 1.82l.35 3.83"></path></svg>
+            <span class="">{formatTime(totalTrackedTime)}</span>
+          </div>
+        {/if}
         {#if card.due}
-          <div class={card.isComplete ? "flex items-center p-1 rounded mr-2 text-white bg-green-500" : card.due < new Date().getTime() ? "flex items-center p-1 rounded mr-2 text-white bg-orange-600" : "flex items-center p-1 rounded mr-1"}>
+          <div class={card.isComplete ? "flex items-center rounded text-white bg-green-500 py-1" : card.due < new Date().getTime() ? "flex items-center rounded text-white bg-orange-600 py-1" : "flex items-center rounded py-1"}>
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><path d="M12 6v6l4 2"/></svg>
             <span class="ml-1">{formatDate(card.due)}</span>
           </div>
@@ -76,9 +108,19 @@
       </div>
     </div>
   {/if}
-  <div class="absolute bottom-0 right-0 hidden group-hover:block">
-    <div class="flex items-center p-2 rounded" title="Start time tracking" on:click={handleTimeTracking}>
-      <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="7"></circle><polyline points="12 9 12 12 13.5 13.5"></polyline><path d="M16.51 17.35l-.35 3.83a2 2 0 0 1-2 1.82H9.83a2 2 0 0 1-2-1.82l-.35-3.83m.01-10.7l.35-3.83A2 2 0 0 1 9.83 1h4.35a2 2 0 0 1 2 1.82l.35 3.83"></path></svg>
+  {#if isZen}
+    <div class="absolute bottom-0 right-0 group-hover:block" class:hidden={!stopwatchStarted}>
+      <button class="flex items-center p-1 bg-indigo-500 hover:bg-indigo-600 rounded-tl-sm rounded-br-sm text-white" title="Start time tracking" on:click={handleStopwatchStart}>
+        {#if stopwatchStarted}
+          <div transition:fly="{{ x: 20 }}" class="relative ml-1 mr-1.5">
+            <span class="flex h-2 w-2">
+              <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-white opacity-75"></span>
+              <span class="relative inline-flex rounded-full h-2 w-2 bg-white"></span>
+            </span>
+          </div>
+        {/if}
+        <span class="font-mono">{formatForStopwatch(seconds)}</span>
+      </button>
     </div>
-  </div>
+  {/if}
 </div>
