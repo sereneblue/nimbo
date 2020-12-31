15741bbfba4495179ac4f41ba002be54e1ad9788
sereneblue <sereneblue@users.noreply.github.com>
2020-12-30 19:54:05 -0500

    build: update purgecss extractor to work with svelte class directive

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,24 @@ module.exports = {
     mode: 'all',
     content: [
       "./src/**/*.svelte"
-    ]
+    ],
+    options: {
+      defaultExtractor: (content) => {
+        const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g)
+        const matchedTokens = []
+        let match = regExp.exec(content)
+
+        while (match) {
+          if (match[0].startsWith('class:')) {
+            matchedTokens.push(match[0].substring(6))
+          } else {
+            matchedTokens.push(match[0])
+          }
+          match = regExp.exec(content)
+        }
+        return matchedTokens
+      }
+    }
   },
   theme: {
     customForms: theme => ({
